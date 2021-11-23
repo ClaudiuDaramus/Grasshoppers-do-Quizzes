a6749e46175e15c8d24d444c78738cb2ebb5a3f1
Claudiu Daramus <38111672+ClaudiuDaramus@users.noreply.github.com>
2021-11-23 17:58:30 +0200

    changes

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
 const { port } = require('./config/express');
-const authorizationMiddleware = require('./middlewares/authorization');
-const loginHandler = require('./controllers/login');
-const { getAllUsers, getUserById, createUser, updateUser, deleteUser } = require('./controllers/users');
-
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./graphql');
 
 const app = express();
-app.use(bodyParser.json());
-
-app.post("/login", loginHandler);
-
-app.get("/users", getAllUsers);
-app.get("/users/:id", getUserById);
-app.post("/users", createUser);
-app.put("/users/:id", updateUser);
-app.delete("/user/:id", deleteUser);
 
 
 app.use("/graphql", graphqlHTTP((req, res, params) => {
