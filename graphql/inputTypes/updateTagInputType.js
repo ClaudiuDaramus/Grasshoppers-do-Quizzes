948b852f7bec7a7cb0e7e7a52902eab9af79d8df
ClaudiuDaramus <38111672+ClaudiuDaramus@users.noreply.github.com>
2022-01-19 17:10:37 +0200

    bug fixes

--- graphql/inputTypes/updateTagInputType.js
+++ graphql/inputTypes/updateTagInputType.js
@@ -1,8 +1,11 @@
-const { GraphQLInputObjectType, GraphQLNonNull, GraphQLString } = require("graphql");
+const { GraphQLInputObjectType, GraphQLNonNull, GraphQLString, GraphQLID} = require("graphql");
 
 const updateTagInputType = new GraphQLInputObjectType({
   name: 'updateTagInput',
   fields: {
+    id: {
+      type: new GraphQLNonNull(GraphQLID),
+    },
     title: {
       type: GraphQLString,
     },
